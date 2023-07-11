f2c514668d6fcc2b3d6d51a7c85a52c86a6ed351
julio_filizzola <juliofilizzola@hotmail.com>
2023-07-11 17:06:17 -0300

    feat: add add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { OfferModule } from './modules/offer/offer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    OfferModule,
+  ],
   controllers: [],
   providers: [AppService],
 })
