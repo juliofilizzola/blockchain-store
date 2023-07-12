3d09f5b6f9bc9d9f7e24e28a98b6c1b262ad7913
julio_filizzola <juliofilizzola@hotmail.com>
2023-07-12 17:01:25 -0300

    feat:create func  purchase

--- src/modules/purchase/dto/create-purchase.dto.ts
+++ src/modules/purchase/dto/create-purchase.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePurchaseDto {}
+import { MethodPaymant } from '@prisma/client';
+import { IsEnum, IsInt, IsNotEmpty, IsPositive, IsUUID } from 'class-validator';
+
+export class CreatePurchaseDto {
+  @IsUUID('4', { message: 'uuid user is invalid' })
+  @IsNotEmpty({ message: 'uuid user is required' })
+  userId: string;
+
+  @IsUUID('4', { message: 'uuid offer is invalid' })
+  @IsNotEmpty({ message: 'uuid offer is required' })
+  offerId: string;
+
+  @IsInt({
+    message: 'quantity has be int',
+  })
+  @IsPositive({
+    message: 'quantity has be positive',
+  })
+  quantity: number;
+
+  @IsInt({
+    message: 'amount has be int',
+  })
+  @IsPositive({
+    message: 'amount has be positive',
+  })
+  amount: number;
+
+  @IsEnum(MethodPaymant)
+  @IsNotEmpty({
+    message: 'method payment is required',
+  })
+  methodPayment: MethodPaymant;
+}
--- src/modules/purchase/purchase.service.ts
+++ src/modules/purchase/purchase.service.ts
@@ -1,26 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
-import { UpdatePurchaseDto } from './dto/update-purchase.dto';
+import { PrismaService } from '../../prisma/prisma.service';
+import { nextDate } from '../../../utils/date';
 
 @Injectable()
 export class PurchaseService {
-  create(createPurchaseDto: CreatePurchaseDto) {
-    return 'This action adds a new purchase';
-  }
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(createPurchaseDto: CreatePurchaseDto) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        id: createPurchaseDto.userId,
+      },
+    });
 
-  findAll() {
-    return `This action returns all purchase`;
-  }
+    if (!user) {
+      throw new NotFoundException({
+        message: 'user not found',
+      });
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} purchase`;
-  }
+    const offer = await this.prismaService.offer.findFirst({
+      where: {
+        id: createPurchaseDto.offerId,
+      },
+      include: {
+        wallet: true,
+      },
+    });
 
-  update(id: number, updatePurchaseDto: UpdatePurchaseDto) {
-    return `This action updates a #${id} purchase`;
-  }
+    if (!offer) {
+      throw new NotFoundException({
+        message: 'offer not found',
+      });
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} purchase`;
+    if (offer.expiration.toString() > nextDate() || !offer.active) {
+      throw new BadRequestException({
+        message: 'offer invalid',
+      });
+    }
+
+    if (createPurchaseDto.quantity > offer.quantity) {
+      throw new BadRequestException({
+        message: 'quantity above available',
+      });
+    }
+
+    return this.prismaService.$transaction(async (prisma) => {
+      if (createPurchaseDto.quantity === offer.quantity) {
+        await prisma.offer.update({
+          where: {
+            id: offer.id,
+          },
+          data: {
+            active: false,
+            quantity: 0,
+          },
+        });
+      } else {
+        await prisma.offer.update({
+          where: {
+            id: offer.id,
+          },
+          data: {
+            quantity: offer.quantity - createPurchaseDto.quantity,
+          },
+        });
+      }
+
+      await prisma.wallet.update({
+        where: {
+          id: offer.wallet.id,
+        },
+        data: {
+          balance: offer.wallet.balance + createPurchaseDto.amount,
+        },
+      });
+
+      return prisma.purchase.create({
+        data: {
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          amountPaid: createPurchaseDto.amount,
+          quantity: createPurchaseDto.quantity,
+          methodPayment: createPurchaseDto.methodPayment,
+          offer: {
+            connect: {
+              id: offer.id,
+            },
+          },
+        },
+      });
+    });
   }
 }
